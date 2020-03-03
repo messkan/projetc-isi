e6eeb81713aba809f82a2f81296de0f883189b0c
Iskander Ben Khelil <40677488+messkan@users.noreply.github.com>
2020-03-04 00:14:38 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,14 @@
 const Sequelize = require('sequelize');
 
+
 // instance of Sequelize
 const database = new Sequelize({
-    database: 'isi_sequelize',
-    username: 'root',
-    password: '',
-    dialect: 'mysql',
+    database: 'daqgf4am4gulqo',
+    host : 'ec2-52-23-14-156.compute-1.amazonaws.com',
+    
+    username: 'bedyfsmorwvecd',
+    password: '9c2bf359f7c84c40a511171efc93e285b092ac0fd78adab9a570898c02f0ab84',
+    dialect: 'postgres',
 });
 
 module.exports = database;
